50f6130f79a984da6d37b790f0c25f92aeac0b60
Taran Mand <tsmand@yahoo.com>
2019-08-08 00:15:04 +1000

    Added Windows Sound beep

--- myPython.py
+++ myPython.py
@@ -126,3 +126,8 @@ print("")
 print("Honey Singh")
 print("New PAtch2")
 print("New PAtch3")
+
+import winsound
+frequency = 2500  # Set Frequency To 2500 Hertz
+duration = 1000  # Set Duration To 1000 ms == 1 second
+winsound.Beep(frequency, duration)
