d6977228d466e1d658186d98b242b22d2cf8fe58
Taran Mand <tsmand@yahoo.com>
2019-08-17 17:10:01 +1000

    Word guessing complete

--- hangman_test.py
+++ hangman_test.py
@@ -13,6 +13,11 @@ maskedWord = ""
 newWord = ""
 #alreadyGen = str(9)
 
+alreadyGuessed = ""
+isGuessComplete = False
+numOfGuessesLeft = 7
+
+
 while i <= m:
 	r = random.randrange(0,l-1)
 	print(str(r) + " Random Num")
@@ -49,40 +54,32 @@ print(maskedWord_lst)
 
 #print(userInput)
 #if userInput in maskedLetters:
-while True:
-	print("Make a guess. Enter Alphabet: ")
-	print("-----------------------------")
-	userInput = str(input())
-	userInput = userInput.strip()
-	userInput = userInput.upper()
-	if userInput.isalpha() and len(userInput) == 1:
-		print("Input is Valid")
-		break
-	else:
-		print("Please only enter a single Alphabet:")
-		continue
 
-print(">>>>>>>>>>>")
-if userInput in maskedLetters:
-	print("Good Guess.")
-	alphaNum = word_lst.count(userInput)
-	#print("Num times exists: " + str(alphaNum))
-	if alphaNum == 1:
-		xIndex = word_lst.index(userInput)
-		maskedWord_lst[xIndex] = word_lst[xIndex]
-		print(maskedWord_lst)
-		for x in maskedWord_lst:
-			newWord = newWord + x
-		print("New Word After Guess: " + newWord)
-	else:
-		'''rInd = 0
-		while alphaNum != 0:
-			xIndex = word_lst.index(userInput)
-			maskedWord_lst[xIndex] = word_lst[xIndex]
-			#make a sub word after that position of that alphabet.
-			subWord = word_lst[xIndex + 1:]
-			rInd = subWord.index(userInput)
-			alphaNum = alphaNum - 1'''
+
+while numOfGuessesLeft > 0 and isGuessComplete == False:
+	while True:
+		print("Already Guessed: " + alreadyGuessed)
+		print("Make a guess. Enter Alphabet: ")
+		print("-----------------------------")
+		userInput = str(input())
+		userInput = userInput.strip()
+		userInput = userInput.upper()
+		if userInput.isalpha() and len(userInput) == 1:
+			print("Input is Valid")
+			if userInput in alreadyGuessed:
+				print("You already guessed this Alphabet earlier...")
+				continue
+			else:
+				alreadyGuessed = alreadyGuessed + userInput
+				break
+		else:
+			print("Please only enter a single Alphabet:")
+			continue
+
+	print(">>>>>>>>>>>")
+
+	if userInput in maskedLetters:
+		print("Good Guess.")
 		i = 0
 		for a in maskedWord_lst:
 			if word_lst[i] == userInput:
@@ -92,6 +89,14 @@ if userInput in maskedLetters:
 		for x in maskedWord_lst:
 			newWord = newWord + x
 		print("New Word After Guess: " + newWord)
+		if newWord == str1:
+			print("You fully guessed the word. You WIN *****")
+			isGuessComplete = True
+		else:
+			newWord = ""
 
-else:
-	print("Wrong guess")
+	else:
+		numOfGuessesLeft = numOfGuessesLeft - 1
+		print("Wrong guess. Chances remaining: " + str(numOfGuessesLeft))
+		if numOfGuessesLeft == 0:
+			print("GAME OVER ###### ----- You are a LOSER")
